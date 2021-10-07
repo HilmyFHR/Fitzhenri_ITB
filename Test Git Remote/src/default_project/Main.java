b5e4957ca770a4c68f61426cae53d0abbe3dcc3e
HilmyFHR <fitzhenri.fhr02@gmail.com>
2021-10-07 23:44:01 +0700

    test Git Remote

---
+++ Test Git Remote/src/default_project/Main.java
@@ -0,0 +1,13 @@
+package default_project;
+
+public class Main {
+    
+    public class main(){
+        System.out.println("Namaku Hilmy");
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
